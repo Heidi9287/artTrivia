027f69e3619eca15eea492ba7487f50875069e8a
xiaoxuanlyu <lyu9287@gmail.com>
2023-12-08 20:50:00 -0500

    more errors log out

--- pages/api/card.ts
+++ pages/api/card.ts
@@ -1,29 +1,34 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { Client } from 'pg';
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-
   const client = new Client({
     database: 'forsal',
-    port: 5432 // default PostgreSQL port number
+    port: 5432,
   });
 
-  try {  console.log('API route is called here');
+  try {
+    console.log('API route is called here');
     await client.connect();
     console.log('Connected to database');
-
     const result = await client.query('SELECT * FROM cards');
     const fetchedData = result.rows;
-    // using the client.query() method
-    // send the data back as a JSON response
     res.json({
-      data: fetchedData
+      data: fetchedData,
     });
-  } catch (error) { console.error('Error in API route:', error);
+  } catch (error:any) {
+    console.error('Error in API route:', error);
+
+    // Log additional information about the error
+    console.error('Error code:', error.code);
+    console.error('Error message:', error.message);
+    console.error('Error stack:', error.stack);
+
     res.status(500).json({
-      error: 'Internal server error is this the reason'
+      error: 'Internal server error',
     });
   } finally {
     await client.end();
